a895b0db7147c2f8786d2d1173145e0f79477277
ozheng7 <103291913+ozheng7@users.noreply.github.com>
2022-04-15 17:01:25 -0700

    lab3 part4
    made the file able to deal with empty lines

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,13 +11,27 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+        int charIndex = 0;
         while(currentIndex < markdown.length()) {
+
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            if (currentIndex <= charIndex) {
+                charIndex ++;
+            }
+            else {
+                charIndex = currentIndex;
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
+
+            if (charIndex == markdown.length() - 1) {
+                return toReturn;
+            }
+
+            //toReturn.add(markdown.substring(openParen + 1, closeParen));
 
             System.out.println(currentIndex);
         }
